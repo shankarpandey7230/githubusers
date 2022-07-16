288fab175a0029c2a144c88b2e38e27d9cfb7ec8
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-07-17 05:20:36 +1000

    updating search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,34 @@ import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState('');
+  // get things from globalcontext
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    if(user){
+      // more logic to come
+      //optional 
+      //setUser('');
+    }
+    }
+  return (
+    <section className='section' >
+      <Wrapper className='section-center'>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <MdSearch />
+            <input type ="text" placeholder='Enter Github 
+            user' value={user} onChange={(e)=>setUser(e.target.value)}/>
+            <button type = 'submit'>Search</button>
+          </div>
+        </form>
+        <h3>requests: 60/60</h3>
+
+
+      </Wrapper>
+
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
