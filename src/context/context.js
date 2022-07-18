7f42ca9b4f27c1266b46900d4c15225bed703366
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-07-18 19:09:21 +1000

    LodingSpinner

--- src/context/context.js
+++ src/context/context.js
@@ -16,12 +16,12 @@ const GithubProvider = ({children}) =>{
   const [followers, setFollowers]= useState(mockFollowers);
   // request loading
   const [requests, setRequests]= useState(0);
-  const [loading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   //error
   const [error, setError]= useState({show:false, msg:""})
   const searchGithubUser = async (user)=>{
    toggleError();
-   // setLoading (true)
+   setIsLoading (true)
    const response = await axios(`${rootUrl}/users/${user}`).
    catch(err=>console.log(err))
    console.log(response);
@@ -32,7 +32,8 @@ const GithubProvider = ({children}) =>{
    else{
     toggleError(true,'there is no user with that username:')
    }
-
+    checkRequests();
+    setIsLoading(false);
   }
   //check rate
   const  checkRequests =()=>{
@@ -59,7 +60,7 @@ const GithubProvider = ({children}) =>{
     
   return (
 
-  <GithubContext.Provider value={{githubUser,repos, followers,requests, error,searchGithubUser}}>{children}</GithubContext.Provider>  
+  <GithubContext.Provider value={{githubUser,repos, followers,requests, error,searchGithubUser,isLoading}}>{children}</GithubContext.Provider>  
   );
 };
 export  {GithubProvider, GithubContext};
